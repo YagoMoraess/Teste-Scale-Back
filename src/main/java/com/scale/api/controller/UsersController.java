b7c14ac0cc96274879f36c5456b98a6a1d4c6cae
Yago <yago.mbarreto@hotmail.com>
2023-02-03 16:10:17 -0300

    Arrumando problema com o CORS e adicionando tag get

--- src/main/java/com/scale/api/controller/UsersController.java
+++ src/main/java/com/scale/api/controller/UsersController.java
@@ -3,6 +3,8 @@ package com.scale.api.controller;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +13,8 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/users")
 public class UsersController {
 	
+	@CrossOrigin
+	@GetMapping
 	public Optional<String> GetUsers() {
 		RestTemplate restTemplate = new RestTemplate();
 	    ResponseEntity<String> response = restTemplate.getForEntity("https://reqres.in/api/users", String.class);

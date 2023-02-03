5d77da84a6e511a7da09a2ab3c3d92950e4bc59f
Yago <yago.mbarreto@hotmail.com>
2023-02-03 15:56:30 -0300

    Criando projeto e desenvolvendo controller que consome API users

---
+++ src/main/java/com/scale/api/TesteScaleApplication.java
@@ -0,0 +1,13 @@
+package com.scale.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteScaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteScaleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/scale/api/controller/UsersController.java
@@ -0,0 +1,19 @@
+package com.scale.api.controller;
+
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+	
+	public Optional<String> GetUsers() {
+		RestTemplate restTemplate = new RestTemplate();
+	    ResponseEntity<String> response = restTemplate.getForEntity("https://reqres.in/api/users", String.class);
+	    return Optional.of(response.getBody());
+	}
+}
